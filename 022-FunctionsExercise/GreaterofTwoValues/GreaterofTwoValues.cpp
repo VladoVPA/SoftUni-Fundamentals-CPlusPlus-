26fe79e18602a5d5a0d89d3175f5aa4ca1e610fd
Vladimir Astardzhiev <v_astardjiev@abv.bg>
2023-11-17 00:00:14 +0200

    Exercise 022
    Exercise 022

--- 022-FunctionsExercise/GreaterofTwoValues/GreaterofTwoValues.cpp
+++ 022-FunctionsExercise/GreaterofTwoValues/GreaterofTwoValues.cpp
@@ -1,6 +1,50 @@
 #include <iostream>
+#include <string>
+using namespace std;
+
+void compareInts() {
+    int a1, a2;
+    cin >> a1 >> a2;
+
+    if (a1 >= a2)
+        cout << a1;
+    else
+        cout << a2;
+}
+
+void compareChars() {
+    char a1, a2;
+    cin >> a1 >> a2;
+
+    if (a1 >= a2)
+        cout << a1;
+    else
+        cout << a2;
+}
+
+void compareStrings() {
+    string a1, a2;
+
+    cin.ignore();
+    getline (cin, a1);
+    getline (cin, a2);
+
+    if (a1 >= a2)
+        cout << a1;
+    else
+        cout << a2;
+}
 
 int main()
 {
-    
+    string type;
+    cin >> type;
+
+    if (type == "int")
+        compareInts();
+    else if (type == "char")
+        compareChars();
+    else if (type == "string")
+        compareStrings();
+
 }
